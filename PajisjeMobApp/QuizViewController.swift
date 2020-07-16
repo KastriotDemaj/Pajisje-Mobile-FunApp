4e44f9227708af768fa69c582d5d9649ddca7ff8
Kastriot <kastriotdemaj@gmail.com>
2020-07-16 13:53:26 +0200

    Korrigjimi i disa gabimeve logjike ne funksionim ke kuizit!

--- PajisjeMobApp/QuizViewController.swift
+++ PajisjeMobApp/QuizViewController.swift
@@ -36,45 +36,124 @@ class QuizViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-    
+    var RandomNumber = 1
     func Random(){
-       var RandomNumber = arc4random()%4
-        RandomNumber += 1
         
         switch RandomNumber {
         case 1:
-            Pyetja.text = "What is the capital of Japan"
+            Pyetja.text = "Cili eshte kryeqyteti i Japonise?"
             Pergjigja1.setTitle("Delhi",for:UIControl.State.normal)
             Pergjigja2.setTitle("Tokyo",for:UIControl.State.normal)
             Pergjigja3.setTitle("London",for:UIControl.State.normal)
             Pergjigja4.setTitle("Paris",for:UIControl.State.normal)
             PergjigjaSakte = "2"
+            RandomNumber = RandomNumber + 1
             break
         case 2:
-            Pyetja.text = "What is the capital of India"
+            Pyetja.text = "Cili eshte kryeqyteti i Indise?"
             Pergjigja1.setTitle("Delhi",for:UIControl.State.normal)
             Pergjigja2.setTitle("Tokyo",for:UIControl.State.normal)
             Pergjigja3.setTitle("London",for:UIControl.State.normal)
             Pergjigja4.setTitle("Paris",for:UIControl.State.normal)
             PergjigjaSakte = "1"
+            RandomNumber = RandomNumber + 1
             break
         case 3:
-            Pyetja.text = "What is the capital of France"
+            Pyetja.text = "Cili eshte kryeqyteti i Frances?"
             Pergjigja1.setTitle("Delhi",for:UIControl.State.normal)
             Pergjigja2.setTitle("Tokyo",for:UIControl.State.normal)
             Pergjigja3.setTitle("London",for:UIControl.State.normal)
             Pergjigja4.setTitle("Paris",for:UIControl.State.normal)
             PergjigjaSakte = "4"
+            RandomNumber = RandomNumber + 1
             break
         case 4:
-            Pyetja.text = "What is the capital of United Kingdom"
+            Pyetja.text = "Cili eshte kryeqyteti i Anglise?"
             Pergjigja1.setTitle("Delhi",for:UIControl.State.normal)
             Pergjigja2.setTitle("Tokyo",for:UIControl.State.normal)
             Pergjigja3.setTitle("London",for:UIControl.State.normal)
             Pergjigja4.setTitle("Paris",for:UIControl.State.normal)
             PergjigjaSakte = "3"
+            RandomNumber = RandomNumber + 1
+            break
+        case 5:
+            Pyetja.text = "Cila nuk eshte peme?"
+            Pergjigja1.setTitle("Portokalli",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Banania",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Limoni",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Patatja",for:UIControl.State.normal)
+            PergjigjaSakte = "4"
+            RandomNumber = RandomNumber + 1
+            break
+        case 6:
+            Pyetja.text = "Permes Gjibraltarit Spanja kalon ne..."
+            Pergjigja1.setTitle("Algjeri",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Maroko",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Tunis",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Libi",for:UIControl.State.normal)
+            PergjigjaSakte = "2"
+            RandomNumber = RandomNumber + 1
+            break
+        case 7:
+            Pyetja.text = "Familjes se bimeve Solonaceae i takon :"
+            Pergjigja1.setTitle("Hudhra",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Qepa",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Karrota",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Duhani",for:UIControl.State.normal)
+            PergjigjaSakte = "4"
+            RandomNumber = RandomNumber + 1
+            break
+        case 8:
+            Pyetja.text = "Kur u fundos anija Titanik?"
+            Pergjigja1.setTitle("1999",for:UIControl.State.normal)
+            Pergjigja2.setTitle("1986",for:UIControl.State.normal)
+            Pergjigja3.setTitle("1912",for:UIControl.State.normal)
+            Pergjigja4.setTitle("1894",for:UIControl.State.normal)
+            PergjigjaSakte = "3"
+            RandomNumber = RandomNumber + 1
+            break
+        case 9:
+            Pyetja.text = "Aurum nga latinishtja eshte emertim per :"
+            Pergjigja1.setTitle("Argjendin",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Arin",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Bronzin",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Zinkun",for:UIControl.State.normal)
+            PergjigjaSakte = "2"
+            RandomNumber = RandomNumber + 1
+            break
+        case 10:
+            Pyetja.text = "Ne shekullin e peste Herodoti i quajtin me e emrin Mont Skardus..."
+            Pergjigja1.setTitle("Karpatet",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Alpnet",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Malet e Sharrit",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Himalajet",for:UIControl.State.normal)
+            PergjigjaSakte = "3"
+            RandomNumber = RandomNumber + 1
+            break
+        case 11:
+            Pyetja.text = "Cila nga keto kafsh eshte gjitar?"
+            Pergjigja1.setTitle("Lopa",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Krokodili",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Pula",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Peshku",for:UIControl.State.normal)
+            PergjigjaSakte = "1"
+            RandomNumber = RandomNumber + 1
+            break
+        case 12:
+            Pyetja.text = "Burimi i ujit te ngrohte nentokesor quhet..."
+            Pergjigja1.setTitle("Vezir",for:UIControl.State.normal)
+            Pergjigja2.setTitle("Vajzir",for:UIControl.State.normal)
+            Pergjigja3.setTitle("Gejzer",for:UIControl.State.normal)
+            Pergjigja4.setTitle("Uje mineral",for:UIControl.State.normal)
+            PergjigjaSakte = "3"
+            RandomNumber = RandomNumber + 1
             break
         default:
+            Pyetja.text = "Nuk ka me pyetje"
+            Pergjigja1.isHidden = true
+            Pergjigja2.isHidden = true
+            Pergjigja3.isHidden = true
+            Pergjigja4.isHidden = true
             break
         }
         
